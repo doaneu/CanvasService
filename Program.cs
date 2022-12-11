50713356cab367f52c3a59a59feaae9b096c5a8d
Chris Craven <Chris.Craven@doane.edu>
2022-12-11 10:13:54 -0600

    Version 2.0.0

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CanvasService.Tasks;
+using Conductor.Client.Extensions;
+using Conductor.Client.Interfaces;
 
 namespace CanvasService
 {
@@ -13,14 +8,47 @@ namespace CanvasService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddConductorWorker(ConductorConnection.Instance.Configuration);
+
+                    //Add Tasks here
+                    services.AddConductorWorkflowTask<CheckCanvasUserExistsTask>();
+                    services.AddConductorWorkflowTask<CreateCanvasUserTask>();
+                    services.AddConductorWorkflowTask<UpdateCanvasUserTask>();
+                    services.AddConductorWorkflowTask<DeleteCanvasUserTask>();
+
+                    services.AddHostedService<WorkflowsWorkerService>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    internal class WorkflowsWorkerService : BackgroundService
+    {
+        private readonly IWorkflowTaskCoordinator workflowTaskCoordinator;
+        private readonly IEnumerable<IWorkflowTask> workflowTasks;
+
+        public WorkflowsWorkerService(
+            IWorkflowTaskCoordinator workflowTaskCoordinator,
+            IEnumerable<IWorkflowTask> workflowTasks
+        )
+        {
+            this.workflowTaskCoordinator = workflowTaskCoordinator;
+            this.workflowTasks = workflowTasks;
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        protected override async System.Threading.Tasks.Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            foreach (var worker in workflowTasks)
+            {
+                workflowTaskCoordinator.RegisterWorker(worker);
+            }
+            // start all the workers so that it can poll for the tasks
+            await workflowTaskCoordinator.Start();
+        }
     }
 }
